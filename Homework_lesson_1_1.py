982d25c4937784fd3d9fcfb5dd5f041a9a6ca6eb
Roman <nemec_r@mail.ru>
2021-05-26 07:05:38 +0300

    HomeWork is done

---
+++ Homework_lesson_1_1.py
@@ -0,0 +1,11 @@
+# 1.	Поработайте с переменными, создайте несколько, выведите на экран. Запросите у пользователя некоторые числа и
+# строки и сохраните в переменные, затем выведите на экран.
+number_int = int(input("Введите произвольное целое число: "))
+print(type(number_int))
+print(number_int)
+number_float = input('Введите дробное число: ')
+print(type(number_float))
+print(number_float)
+string_new = input('Введите произвольную фразу или предложение: ')
+print(type(string_new))
+print(string_new)
